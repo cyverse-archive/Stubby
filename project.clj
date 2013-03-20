738e1814b8217bb3966bd82cc472a600c291143b
Tony Edgin <tedgin@iplantcollaborative.org>
2013-03-20 12:58:50 -0700

    clojure 1.5.1 and upped version number

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject stubby "0.1.0-SNAPSHOT"
+(defproject stubby "0.1.1-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.0.1"]
                  [compojure "1.1.1"]
                  [slingshot "0.10.1"]
